61831e1efc7f589c744a40fa84eecdeccd80c0b3
Simon Ingeson <simon@argylehq.com>
2019-05-01 16:23:31 -0400

    prettier + node.js update

--- index.js
+++ index.js
@@ -1,50 +1,53 @@
-var http = require('http')
-var fs = require('fs')
-var path = require('path')
-var crypto = require('crypto')
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const crypto = require("crypto");
 
-var rgx = /^\/bytes\/([0-9]{1,4})\/(base64|hex)$/i
+const rgx = /^\/bytes\/([0-9]{1,4})\/(base64|hex)$/i;
 
-function requestListener (req, res) {
-  if (req.method !== 'GET') {
-    res.writeHead(405, {'Allow': 'GET', 'Content-Type': 'text/plain'})
-    res.end('Method Not Allowed: ' + req.method)
-    return
+function requestListener(req, res) {
+  if (req.method !== "GET") {
+    res.writeHead(405, { Allow: "GET", "Content-Type": "text/plain" });
+    res.end("Method Not Allowed: " + req.method);
+    return;
   }
 
-  if (req.url === '/') {
-    var indexFile = fs.createReadStream(path.join(__dirname, 'index.html'), 'utf8')
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    indexFile.pipe(res)
-    return
+  if (req.url === "/") {
+    const indexFile = fs.createReadStream(
+      path.join(__dirname, "index.html"),
+      "utf8"
+    );
+    res.writeHead(200, { "Content-Type": "text/html" });
+    indexFile.pipe(res);
+    return;
   }
 
   if (!rgx.test(req.url)) {
-    res.writeHead(404, {'Content-Type': 'text/plain'})
-    res.end('Not Found: ' + req.url)
-    return
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Not Found: " + req.url);
+    return;
   }
 
-  var parts = req.url.match(rgx)
-  var count = parseInt(parts[1], 10)
-  var format = parts[2].toLowerCase()
+  const parts = req.url.match(rgx);
+  const count = parseInt(parts[1], 10);
+  const format = parts[2].toLowerCase();
 
   if (isNaN(count) || count < 1) {
-    res.writeHead(400, {'Content-Type': 'text/plain'})
-    res.end('Bad Request: Byte count must be an integer and greater than 1')
-    return
+    res.writeHead(400, { "Content-Type": "text/plain" });
+    res.end("Bad Request: Byte count must be an integer and greater than 1");
+    return;
   }
 
-  crypto.randomBytes(count, function (err, buffer) {
+  crypto.randomBytes(count, function(err, buffer) {
     if (err) {
-      res.writeHead(500, {'Content-Type': 'text/plain'})
-      res.end('Server Error: ' + err.message)
-      return
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Server Error: " + err.message);
+      return;
     }
 
-    res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.end(buffer.toString(format))
-  })
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end(buffer.toString(format));
+  });
 }
 
-http.createServer(requestListener).listen(process.env.PORT || 3000)
+http.createServer(requestListener).listen(process.env.PORT || 3000);
